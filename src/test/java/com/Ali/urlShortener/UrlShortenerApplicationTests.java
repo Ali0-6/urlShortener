fd37d63d496b5afa675e89a2feb5620ece68a51a
Ali <a.ali.mostafa06@gmail.com>
2026-07-20 08:58:25 +0100

    Fixed test file for urlshortenerapplication to use test profile

--- src/test/java/com/Ali/urlShortener/UrlShortenerApplicationTests.java
+++ src/test/java/com/Ali/urlShortener/UrlShortenerApplicationTests.java
@@ -2,8 +2,10 @@ package com.Ali.urlShortener;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UrlShortenerApplicationTests {
 
 	@Test
